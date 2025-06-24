f3816f5e2f2c835fe8e4219b414e5ffc007bf7e9
akshat799 <gulati.akshat799@gmail.com>
2025-06-24 01:16:36 -0300

    feat(docker): add complete dev and prod Docker setup for backend and frontend

--- backend/server.js
+++ backend/server.js
@@ -28,8 +28,8 @@ app.get('/', (req, res) => {
 
 // START SERVER  Only run server if not in test mode
 if (process.env.NODE_ENV !== 'test') {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
+  const PORT = process.env.PORT || 8080;
+  app.listen(PORT, '0.0.0.0',() => {
     console.log(`Server running on port ${PORT}`);
   });
 }
