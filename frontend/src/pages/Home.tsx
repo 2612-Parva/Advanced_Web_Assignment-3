fced16e49ca97764e26e41766f9f44530c58e84c
Parva Mineshbhai Patel <pr548271@dal.ca>
2025-07-24 16:31:07 -0300

    Modified Code

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import React, { useEffect } from 'react';
 import Header from '../components/Header';
 import HeroSection from '../components/HeroSection';
 import AboutSection from '../components/AboutSection';
@@ -7,14 +6,8 @@ import ServicesSection from '../components/ServicesSection';
 import TestimonialsSection from '../components/TestimonialsSection';
 import Footer from '../components/Footer';
 
-import { useAppSelector} from '../redux/hooks';
 
 const Home: React.FC = () => {
-  const user = useAppSelector((state) => state.user);
-  useEffect(() => {
-      console.log('Current user:', user);
-  }, [user]);
-
   return (
     <>
       <Header />
